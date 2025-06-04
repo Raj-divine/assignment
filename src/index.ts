d9d19d51073380b51f37c12f01b3638206434122
Raj-divine <tarunkush0510@gmail.com>
2025-06-04 21:11:17 +0530

    storing currently loggedin users in users.json file

--- src/index.ts
+++ src/index.ts
@@ -66,4 +66,98 @@ const getUsers = async () => {
     }
 }
 
-getUsers()
+const getTokens = async () => {
+    try {
+        const cookies = await getCookies();
+        if (!cookies) throw new Error('Failed to get cookies');
+        const res = await fetch('https://challenge.sunvoy.com/settings/tokens', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Cookie': cookies
+            }
+        })
+
+        const html = await res.text()
+
+        const hiddenInputValues: { [id: string]: string } = {};
+
+        // Regex to find all hidden input tags
+        const hiddenInputRegex = /<input[^>]*type="hidden"[^>]*>/gi;
+        const matches = html.match(hiddenInputRegex);
+
+        if (matches) {
+            matches.forEach(inputTag => {
+                // Extract id and value from each input tag
+                const idMatch = inputTag.match(/id="([^"]*)"/);
+                const valueMatch = inputTag.match(/value="([^"]*)"/);
+
+                if (idMatch && valueMatch) {
+                    const id = idMatch[1];
+                    const value = valueMatch[1];
+                    hiddenInputValues[id] = value;
+                }
+            });
+        }
+        return hiddenInputValues;
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+const generateCheckCodeString = (values: { [key: string]: string }) => {
+    if (!values) {
+        console.error("Values are not provided");
+        process.exit(1);
+    }
+    const timestamp = Math.floor(Date.now() / 1e3)
+    const checkCodeData: { [key: string]: string } = {
+        ...values,
+        timestamp: timestamp.toString(),
+    }
+
+    const dataString = Object.keys(checkCodeData).sort().map(data => `${data}=${encodeURIComponent(checkCodeData[data])}`).join("&")
+    const checkCode = crypto.createHmac("sha1", "mys3cr3t").update(dataString).digest('hex').toUpperCase(); //The secret is hard coded here but it should be stored in .env file in a production env.
+    return `${dataString}&checkcode=${checkCode}`;
+}
+
+const getCurrentlyLoggedInUser = async () => {
+    try {
+        const tokens = await getTokens();
+        const checkCodeString = generateCheckCodeString(tokens);
+        const cookies = await getCookies();
+        if (!tokens || !checkCodeString || !cookies) throw new Error('Failed to get tokens, checkCode or cookies');
+        const res = await fetch('https://api.challenge.sunvoy.com/api/settings', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cookie': cookies
+            },
+            body: new URLSearchParams(checkCodeString)
+        })
+
+        const currentUser = await res.json()
+
+        const existingUsers = JSON.parse(await fs.readFile('users.json', 'utf-8'));
+        existingUsers.push(currentUser);
+        await fs.writeFile('users.json', JSON.stringify(existingUsers, null, 2));
+
+        return getCurrentlyLoggedInUser;
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+const main = async () => {
+    try {
+        await getUsers();
+        await getCurrentlyLoggedInUser()
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+main()
